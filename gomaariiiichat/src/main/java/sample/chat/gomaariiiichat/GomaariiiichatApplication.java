2f6ccff2a4bff168bc0e93895310dcb613fc6ff5
大河原啓太 <ookwrkit@gmail.com>
2020-02-06 04:26:27 +0900

    初期コミット

---
+++ gomaariiiichat/src/main/java/sample/chat/gomaariiiichat/GomaariiiichatApplication.java
@@ -0,0 +1,13 @@
+package sample.chat.gomaariiiichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomaariiiichatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GomaariiiichatApplication.class, args);
+    }
+
+}
---
+++ gomaariiiichat/src/test/java/sample/chat/gomaariiiichat/GomaariiiichatApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.chat.gomaariiiichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomaariiiichatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
